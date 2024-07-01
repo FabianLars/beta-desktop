d1e4b6a3f53909879d35229cec1ad19ee4253d52
FabianLars <fabianlars@fabianlars.de>
2024-07-01 17:19:30 +0200

    up

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::WindowEvent;
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,17 +9,7 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        /* .on_window_event(|w, e| {
-            if let WindowEvent::Resized(_) = e {
-                std::thread::sleep(std::time::Duration::from_nanos(1));
-            }
-        }) */
-        .setup(|app| {
-            let s = Some("string".to_string());
-            let s2 = s.clone();
-            dbg!(&app.config().bundle.license);
-            Ok(())
-        })
+        .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
